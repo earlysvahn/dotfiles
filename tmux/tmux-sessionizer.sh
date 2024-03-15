3310cb259907df208c6e0d296ca1129c2fd2b3b9
Fredrik Svahn <fredrik.svahn@earlybird.se>
2024-03-15 08:05:15 +0100

    Changed emojis

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -18,20 +18,20 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	t new-session -s $selected_name -c $selected -n ' vim' \; \
-		new-window -n ' git' \; \
-		send-keys -t $selected_name:' git' 'lazygit' C-m \; \
-		select-window -t $selected_name:' vim' \; \
-		send-keys -t $selected_name:' vim' 'nvim .' C-m
+	t new-session -s $selected_name -c $selected -n '🧛 vim' \; \
+		new-window -n '🌳 git' \; \
+		send-keys -t $selected_name:'🌳 git' 'lazygit' C-m \; \
+		select-window -t $selected_name:'🧛 vim' \; \
+		send-keys -t $selected_name:'🧛 vim' 'nvim .' C-m
 	exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	t new-session -ds $selected_name -c $selected -n ' vim' \; \
-		new-window -n ' git' \; \
-		send-keys -t $selected_name:' git' 'lazygit' C-m \; \
-		select-window -t $selected_name:' vim' \; \
-		send-keys -t $selected_name:' vim' 'nvim .' C-m
+	t new-session -ds $selected_name -c $selected -n '🧛 vim' \; \
+		new-window -n '🌳 git' \; \
+		send-keys -t $selected_name:'🌳 git' 'lazygit' C-m \; \
+		select-window -t $selected_name:'🧛 vim' \; \
+		send-keys -t $selected_name:'🧛 vim' 'nvim .' C-m
 fi
 
 if ! { [ -n "$TMUX" ]; }; then
