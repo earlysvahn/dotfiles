01095bd67d13bde3948219a5de5234757bdecde2
Fredrik Svahn <fredrik.svahn@earlybird.se>
2024-11-05 10:04:29 +0100

    moved github to center

--- apps/sketchybar/items/github_issues.sh
+++ apps/sketchybar/items/github_issues.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-sketchybar --add item github_issues right
+sketchybar --add item github_issues center
 
 sketchybar --set github_issues \
   script="$PLUGIN_DIR/github_issues.sh" \
   click_script="$PLUGIN_DIR/github_issues_click.sh" \
-  icon.font="$FONT:Regular:14.0" \
-  padding_right=3 \
-  label.font="$FONT:Bold:14.0" \
+  icon.font="$FONT:Regular:17.0" \
+  label.font="$FONT:Bold:17.0" \
   label.drawing=on \
   update_freq=600 \
   background.drawing=on \
--- apps/sketchybar/items/github_prs.sh
+++ apps/sketchybar/items/github_prs.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-sketchybar --add item github_prs right
+sketchybar --add item github_prs center
 
 sketchybar --set github_prs \
   script="$PLUGIN_DIR/github_prs.sh" \
   click_script="$PLUGIN_DIR/github_prs_click.sh" \
-  icon.font="$FONT:Regular:14.0" \
-  padding_left=3 \
-  label.font="$FONT:Bold:14.0" \
+  icon.font="$FONT:Regular:17.0" \
+  label.font="$FONT:Bold:17.0" \
   label.drawing=on \
   update_freq=300 \
   updates=on \
